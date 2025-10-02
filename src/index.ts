68d2980a5c88e547e798e671c2727efaa5b9d827
Julian Waller <git@julusian.co.uk>
2025-10-02 21:33:52 +0100

    wip: Export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './surface-api/index.js'
 export { Complete, parseColor, assertNever } from './util.js'
 
 export { setupPlugin } from './entrypoint.js'
+
+export type * from '../generated/surface-layout.d.ts'
