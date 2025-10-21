086ef8f72c1a1bb03956cf6c8411ec723ad1b2f8
Julian Waller <git@julusian.co.uk>
2025-10-21 23:18:06 +0100

    fix: manifest validator

--- packages/companion-surface-base/tools/schema-compile.mjs
+++ packages/companion-surface-base/tools/schema-compile.mjs
@@ -5,6 +5,7 @@ import Ajv2020 from 'ajv/dist/2020.js'
 import standaloneCode from 'ajv/dist/standalone/index.js'
 import manifestSchema from '../assets/manifest.schema.json' with { type: 'json' }
 import surfaceLayoutSchema from '../assets/surface-layout.schema.json' with { type: 'json' }
+import { fileURLToPath } from 'url'
 
 // Compile Typescript definitions from the JSON schema
 await $`json2ts --input assets/manifest.schema.json --output generated/manifest.d.ts --additionalProperties=false`
@@ -17,10 +18,12 @@ await $`json2ts --input assets/surface-layout.schema.json --output generated/sur
 	const validate = ajv.compile(manifestSchema)
 	let moduleCode = standaloneCode(ajv, validate)
 
-	moduleCode = `import { createRequire } from 'module';const require = createRequire(import.meta.url);` + moduleCode
-
 	// Now you can write the module code to file
-	fs.writeFileSync(new URL('../generated/validate_manifest.js', import.meta.url), moduleCode)
+	const outputPath = new URL('../generated/validate_manifest.js', import.meta.url)
+	fs.writeFileSync(outputPath, moduleCode)
+
+	// the reference to ajv runtime makes some consumers grumpy, so pre-bundle it with esbuild
+	await $`esbuild --bundle ${fileURLToPath(outputPath)} --outfile=${fileURLToPath(outputPath)} --target=node22 --platform=node --format=esm --allow-overwrite`
 }
 
 {
